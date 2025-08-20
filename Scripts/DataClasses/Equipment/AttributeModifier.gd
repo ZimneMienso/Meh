836d5e84714064cb4dfe309c743085c785559709
Zimny <szpa.general@gmail.com>
2025-08-20 03:06:01 +0200

    Auto Attribute Modifier Resource Naming

--- Scripts/DataClasses/Equipment/AttributeModifier.gd
+++ Scripts/DataClasses/Equipment/AttributeModifier.gd
@@ -1,3 +1,4 @@
+@tool
 class_name AttributeModifier
 extends Resource
 
@@ -6,6 +7,9 @@ enum TYPE {
  MULTIPLIER
 }
 
-@export var attribute_type: Attribute.TYPE = Attribute.TYPE.NULL
+@export var attribute_type: Attribute.TYPE = Attribute.TYPE.NULL:
+	set(value):
+		attribute_type = value
+		resource_name = Attribute.TYPE.find_key(value).capitalize()
 @export var bonus_type: TYPE
 @export var value: float = 0
